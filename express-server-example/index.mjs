da478543d93e4880cbb626a2de4ed283bb3d9f01
dylanthebean <dylan.behan93@gmail.com>
2021-12-06 15:27:28 +0000

    express server todo example

---
+++ express-server-example/index.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+const database = [];
+
+app.post("/todo", (req, res) => {
+  const newTodo = {
+    id: Date.now(),
+    text: req.body.text,
+  };
+
+  database.push(newTodo);
+
+  res.json(newTodo);
+});
+
+app.get("/todo", (req, res) => {
+  res.json(database);
+});
+
+app.listen(8000, () => {
+  console.log(`Listening on port 8000`);
+});
