f5549dcb98325cbbad0f81abedaa4f154601a53d
dylanthebean <dylan.behan93@gmail.com>
2022-01-19 13:09:12 +0000

    pangram

--- Algos and DSA/isPangram.js
+++ Algos and DSA/isPangram.js
@@ -1,5 +1,7 @@
 const abc = "abcdefghijklmnopqrstuvwxyz";
 
+// Method 1
+
 const isPangram = (string) => {
   const newString = string.trim().split(" ").join("").toLowerCase();
 
@@ -21,3 +23,28 @@ const isPangram = (string) => {
 isPangram("The quick Brown fox jumps over the lazy DOG");
 isPangram("abcdefghijklmnopqrstuvwxyz");
 isPangram("abcdefgnopqvwxyzzzzzzzzzzzzzzzzzz");
+
+// Method 2
+
+const isPangram2 = (string) => {
+  const processedString = [...new Set(string.toLowerCase().split(" ").join(""))]
+    .sort()
+    .join("");
+
+  return console.log(abc === processedString);
+};
+
+isPangram2("The quick Brown fox jumps over the lazy DOG");
+isPangram2("abcdefghijklmnopqrstuvwxyz");
+isPangram2("abcdefgnopqvwxyzzzzzzzzzzzzzzzzzz");
+
+// Method 3
+
+const isPangram3 = (string) => {
+  const regexMatch = new Set(string.toLowerCase().match(/[a-z]/gi));
+  return console.log(regexMatch.size === 26);
+};
+
+isPangram3("The quick Brown fox jumps over the lazy DOG");
+isPangram3("abcdefghijklmnopqrstuvwxyz");
+isPangram3("abcdefgnopqvwxyzzzzzzzzzzzzzzzzzz");
